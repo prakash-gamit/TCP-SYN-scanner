e7a378f44af7e20a9a82cd4705dd829e54f00edd
Prakash Gamit <prakashgamit23@gmail.com>
2013-02-01 23:59:10 +0530

    add names of the top ports

--- topports.py
+++ topports.py
@@ -3,14 +3,14 @@
 # List is taken from "Nmap - Scanning the Internet", DEFCON 16
 # presentation by Fyodor
 
-
-topports = [80,
-            23,
-            22,
-            443,
-            3389,
-            445,
-            139,
-            21,
-            135,
-            25]
+           # port   # service name
+topports = [80,     # http
+            23,     # telnet
+            22,     # ssh
+            443,    # https
+            3389,   # ms-term-serv
+            445,    # microsoft-ds
+            139,    # netbios-ssn
+            21,     # ftp
+            135,    # msrpc
+            25]     # smtp
