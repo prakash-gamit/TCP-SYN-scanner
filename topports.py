f505d1673692c43ef294e75770e7dd8bdee77c42
Prakash Gamit <prakashgamit23@gmail.com>
2013-02-01 23:47:01 +0530

    add a list of top ten TCP ports

---
+++ topports.py
@@ -0,0 +1,16 @@
+# List top ten TCP ports
+
+# List is taken from "Nmap - Scanning the Internet", DEFCON 16
+# presentation by Fyodor
+
+
+topports = [80,
+            23,
+            22,
+            443,
+            3389,
+            445,
+            139,
+            21,
+            135,
+            25]
