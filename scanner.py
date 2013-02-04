30cc8bc1acac56cc0f6b6d127ccf5f76140079c4
Prakash Gamit <prakashgamit23@gmail.com>
2013-02-04 21:46:03 +0530

    define global variables

---
+++ scanner.py
@@ -0,0 +1,17 @@
+
+import Queue
+
+# version of program
+version = "v1.0.0"
+
+# target to scan
+target = ""
+
+# total number of threads to create
+totalThreads = 10
+
+verbose = False
+
+# list of ports to scan
+portlist = Queue.Queue()
+
